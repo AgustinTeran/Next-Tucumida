d5a7d8cf4f0f5e08ca7ff01607cc625f091574cc
agustinteran <agustintern@gmail.com>
2023-10-19 14:42:44 -0300

    robots.js

---
+++ front/robots.js
@@ -0,0 +1,10 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/misRestaurantes',
+    },
+    sitemap: 'https://acme.com/sitemap.xml',
+  }
+}
