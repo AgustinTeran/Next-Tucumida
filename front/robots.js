1ceaa4d50d5194c37a6c07c87b7727074f3421f5
agustinteran <agustintern@gmail.com>
2023-10-20 20:35:04 -0300

    sitemap

--- front/robots.js
+++ front/robots.js
@@ -3,8 +3,8 @@ export default function robots() {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/misRestaurantes',
+      disallow: '/misRestaurantes/',
     },
-    // sitemap: 'https://acme.com/sitemap.xml',
+    sitemap: 'https://tucumida.com.ar/public/sitemap.xml',
   }
 }
